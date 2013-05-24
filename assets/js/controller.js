57ffdd1a443bdb3000daedc13f32f6008de391c2
johnqing <csssnow@gmail.com>
2013-05-25 00:28:56 +0800

    up

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -5,4 +5,13 @@ define(['jquery'], function($){
     contTitle.click(function(){
         $(this).next(".constroller-con").slideToggle("slow").siblings(".constroller-con:visible").slideUp("slow");
     });
+    //二级切换
+    var subCont = contCon.find('.controller-panel-nav');
+    subCont.bind('click', function(){
+        var self = $(this),
+            oUL = self.parents('ul'),
+            index = self.index() + 1;
+        oUL.find('li:eq('+index+')').show().siblings(':not(:first)').hide();
+        oUL.css("position","relative").animate({left: ~250}, "slow");
+    });
 });
