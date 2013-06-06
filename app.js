7b5d8ac06e0c6ae13ff3ec72bd61d455e39ce7d2
liuqing <liuqing>
2013-06-06 10:24:12 +0800

    up 增加icon

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.configure(function(){
     app.set('view engine', 'html');
     app.engine('.html', require('ejs').__express);
     app.use(flash());
-    app.use(express.favicon());
+    app.use(express.favicon(__dirname+'/favicon.ico'));
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
